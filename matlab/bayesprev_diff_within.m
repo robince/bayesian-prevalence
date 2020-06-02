9b976c7d520ccb8f4b443e53b8fbea18a6e350e6
Robin Ince <robince@gmail.com>
2020-06-02 11:02:09 +0100

    vectorize and support different a/b as per Jims R code

--- matlab/bayesprev_diff_within.m
+++ matlab/bayesprev_diff_within.m
@@ -28,6 +28,14 @@ if nargin<=7
     Nsamp = 10000;
 end
 
+% default both tests have the same properties
+if length(a)==1
+    a = repmat(a,1,2);
+end
+if length(b)==1
+    b = repmat(b,1,2);
+end
+
 % gamma priors, Dirichlet parameters
 r11 = 1;
 r10 = 1;
@@ -46,34 +54,31 @@ m10 = k10 + r10;
 m01 = k01 + r01;
 m00 = k00 + r00;
 
-
-
 the11d = makedist('Beta','a',m11,'b',m01+m10+m00);
-the11d = the11d.truncate(0,b);
+the11d = the11d.truncate(0,min(b));
 
 the10d = makedist('Beta','a',m10,'b',m01+m00);
 the01d = makedist('Beta','a',m01,'b',m00);
 
-theta_samp = zeros(4,Nsamp);
+the11 = the11d.random(Nsamp,1);
 
-% sample from truncated dirichlet
-for si=1:Nsamp
-    the11 = the11d.random(1,1);
-    theta_samp(1,si) = the11;
-    
-    the10d_trunc = the10d.truncate( max([ (a-the11)./(1-the11) 0 ]), (b-the11)./(1-the11) );
-    u10 = the10d_trunc.random(1,1);
-    the10 = (1-the11).*u10;
-    theta_samp(2,si) = the10;
-    
-    the01d_trunc = the01d.truncate( max([ (a-the11)./(1-the11-the10) 0 ]), min([ (b-the11)./(1-the11-the10) 1]) );
-    the01 = (1-the11-the10).*the01d_trunc.random(1,1);
-    theta_samp(3,si) = the01;
-end
+low = max((a(1)-the11)./(1-the11),0);
+high = (b(1)-the11)./(1-the11);
+zlow = the10d.cdf(low);
+zhigh = the10d.cdf(high);
+z10 = zlow + (zhigh-zlow).*rand(Nsamp,1);
+u10 = the10d.icdf(z10);
+the10 = (1-the11).*u10;
 
-g_diff_samples = (theta_samp(2,:) - theta_samp(3,:))./(b-a);
+low = max((a(2)-the11)./(1-the11-the10), 0);
+high = min((b(2)-the11)./(1-the11-the10), 1);
+zlow = the01d.cdf(low);
+zhigh = the01d.cdf(high);
+z01 = zlow + (zhigh-zlow).*rand(Nsamp,1);
+u01 = the01d.icdf(z01);
+the01 = (1-the11-the10).*u01;
 
-% [min(g_diff_samples) max(g_diff_samples)]
+g_diff_samples = (the11 + the10 - a(1))./(b(1)-a(1)) - (the11 + the01 - a(2))./(b(2)-a(2));
 
 x = linspace(-1,1,200); 
 g_diff_post = ksdensity(g_diff_samples,x);
@@ -86,7 +91,7 @@ post_p = g_diff_post;
 post_x = x;
 post_samples = g_diff_samples;
 if nargin>=5
-    hpi = hpdi(g_diff_samples',100*p);
+    hpi = hpdi(g_diff_samples,100*p);
 end
 
 
